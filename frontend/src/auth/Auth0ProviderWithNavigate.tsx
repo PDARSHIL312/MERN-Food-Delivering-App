826290af0d8d00a1254c4327c925ab33a2b9b59d
PDARSHIL312 <darshilp200300@gmail.com>
2024-06-17 20:40:26 +0530

    message

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+};
+
+export default Auth0ProviderWithNavigate;
